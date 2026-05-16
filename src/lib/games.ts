4b9f662ba95a8168a1fe66e15c0a92087af97021
Marcos Traverso <marcostraverso2003@gmail.com>
2026-05-16 19:26:30 -0400

    Add more games!

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -1,8 +1,10 @@
 export const GAMES = [
   { name: 'LoL NA', value: 'LoL NA' },
+  { name: 'LoL EUW', value: 'LoL EUW' },
   { name: 'LoL PBE', value: 'LoL PBE' },
   { name: 'Starcraft 2', value: 'Starcraft 2' },
+  { name: 'Goose Goose Duck', value: 'Goose Goose Duck' },
   { name: 'Valorant', value: 'Valorant' },
-  { name: 'Overwatch 2', value: 'Overwatch 2' },
+  { name: 'Overwatch', value: 'Overwatch' },
   { name: 'Other', value: 'Other' },
 ] as const
